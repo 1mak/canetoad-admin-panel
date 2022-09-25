7370ee747cd6d5d99cb5d98754336f73e2e0f72a
Torsten <torsten.max.schmidt@gmail.com>
2022-09-26 07:26:12 +1000

    Initial commit

---
+++ src/pages/products/categories/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="category-list">
+  Category list
+</div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/products/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="products">
+  Products
+</div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
